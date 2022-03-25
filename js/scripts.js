d9866c42c5ac87f13294e0b54bdf955b75671df8
Jake Edgar <jakeedgar1012@gmail.com>
2022-03-25 11:25:53 -0700

    finish conditional statements and basic functionality

--- js/scripts.js
+++ js/scripts.js
@@ -18,11 +18,47 @@ $(document).ready(function() {
     } else {
       result += 5;
     }
-    console.log(website);
-    console.log(result);
 
+    if (view === "interface") {
+      result += 1;
+    } else if (view === "design") {
+      result += 3;
+    } else {
+      result += 5;
+    }
 
+    if (common === "javascript") {
+      result += 1;
+    } else if (common === "ruby") {
+      result += 3;
+    } else {
+      result += 5;
+    }
 
-    $("#output").text(result);
+    if (most === "consistent") {
+      result += 1;
+    } else if (most === "creative") {
+      result += 3;
+    } else {
+      result += 5;
+    }
+
+    if (worst === "numbers") {
+      result += 1;
+    } else if (worst === "volume") {
+      result += 3;
+    } else {
+      result += 5;
+    }
+
+    if (result <= 6) {
+      $("#javascript").show();
+    } else if (result <= 16) {
+      $("#ruby").show();
+    } else {
+      $("#c").show();
+    }
+
+    $("#output").show(result);
   });
 });
